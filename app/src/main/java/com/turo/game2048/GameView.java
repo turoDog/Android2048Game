303486822b496c3acbe3e07cd485b897878feec0
turoDog <turodog@foxmail.com>
2016-09-10 22:30:10 +0800

    第三步：在GameView类中增加2048游戏在Android平台的触控交互设计

--- app/src/main/java/com/turo/game2048/GameView.java
+++ app/src/main/java/com/turo/game2048/GameView.java
@@ -2,6 +2,8 @@ package com.turo.game2048;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.GridLayout;
 
 /**
@@ -27,7 +29,64 @@ public class GameView extends GridLayout {
         initGameView();
     }
 
-    private void initGameView(){
+    private void initGameView() {
+        setOnTouchListener(new OnTouchListener() {
+
+            private float startX, startY, offsetX, offsetY;
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        startX = event.getX();
+                        startY = event.getY();
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                        offsetX = event.getX() - startX;
+                        offsetY = event.getY() - startY;
+
+                        if (Math.abs(offsetX) > Math.abs(offsetY)) {
+                            if (offsetX < -5) {
+                                slideLeft();
+                            } else if (offsetX > 5) {
+                                slideRight();
+                            }
+                        } else {
+                            if (offsetY < -5) {
+                                slideUp();
+                            } else if (offsetY > 5) {
+                                slideDown();
+                            }
+                        }
+                        break;
+
+                }
+                return true;
+            }
+        });
+
 
     }
+
+    private void slideLeft() {
+
+    }
+
+    private void slideRight() {
+    }
+
+    private void slideUp(){
+
+    }
+
+    private void slideDown() {
+
+    }
+
 }
+
+
+
+
+
